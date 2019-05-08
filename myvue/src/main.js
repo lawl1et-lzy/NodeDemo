0253bc8a7a331c674468787a822ca1757caab969
林志勇 <linzhiyong@changingedu.com>
2019-05-08 13:31:33 +0800

    【vue】初始化 element UI

--- myvue/src/main.js
+++ myvue/src/main.js
@@ -3,7 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 // 设置代理
 Vue.prototype.http = '/api'
--- myvue/src/pages/HomePage/Index.vue
+++ myvue/src/pages/HomePage/Index.vue
@@ -5,26 +5,9 @@
 </template>
 
 <script>
-import axios from '@/util/Api/Api.js'
 export default {
   name: 'Index',
   mounted () {
-    let data = {
-      'username': 'lawliet9',
-      'password': '123123'
-    }
-    let url = `${this.http}/signUp`
-    axios({
-      method: 'POST',
-      url: url,
-      data
-    })
-      .then(res => {
-        console.log('Axios res', res)
-      })
-      .catch(err => {
-        console.log('Axios err', err)
-      })
   }
 }
 </script>
